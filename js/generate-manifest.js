0993ad6b709d5a0df51672a39625c653efeb9d64
William Wall <williamjwall@gmail.com>
2025-05-14 11:12:07 -0700

    Remove blog related content and update website configuration

--- js/generate-manifest.js
+++
@@ -1,4 +0,0 @@
-// This is just a simple redirector to the actual script
-const path = require('path');
-const actualScriptPath = path.join(__dirname, '../blog/js/generate-manifest.js');
-require(actualScriptPath); 
